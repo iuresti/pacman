b99ad28b4f4ac55d199122ec1a4fe898d55546c3
Iván Uresti <ivan.uresti@gmail.com>
2020-10-12 19:29:39 -0500

    Update Banana.java
    Comentarios

--- src/uaslp/enginering/labs/Map/Banana.java
+++ src/uaslp/enginering/labs/Map/Banana.java
@@ -1,4 +1,4 @@
-package uaslp.enginering.labs.Map;
+package uaslp.enginering.labs.Map; //<<- recuerda que los paquetes por convención se escriben solo con minúsculas
 
 public class Banana extends Fruit{
 }
